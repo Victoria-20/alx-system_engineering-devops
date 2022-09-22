1c2fba18629970a30053f6554bcca70017a96702
Victoria-20 <victoriaocon111@gmail.com>
2022-09-22 13:55:10 +0300

    Redirection

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,1 +1,24 @@
 #!/usr/bin/env bash
+# Configure server using puppet
+package { 'nginx':
+  ensure => 'installed',
+}
+
+file { '/var/www/html/index.html':
+  require => Package['nginx'],
+  content => 'Hello World!',
+}
+
+file_line { 'add-rewrite':
+  ensure  => 'present',
+  require => Package['nginx'],
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'root /var/www/html;',
+  line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH@-TGUlwu4 permanent;',
+  notify  => Service['nginx'],
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => File_line['add-rewrite'],
+}
