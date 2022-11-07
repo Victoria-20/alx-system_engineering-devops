a50201b958d94ba32dfbf17b88e99a042cc0e877
Victoria-20 <victoriaocon111@gmail.com>
2022-11-07 09:18:03 +0300

    0. How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+""" return total number of subscribers"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """ Query reddit API & return no. of subs """
+    if subreddit:
+        res = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'User-Agent': 'Python/requests:APIproject:\
+v1.0.0 (by /u/aaorrico23)'}).json()
+        sub = res.get("data", {}).get("subscribers", 0)
+        return sub
+    return 0
